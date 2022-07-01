d4c0148e0b47e2c0a7a874c427abdb37cb28fb55
MagicManik <manikislam1952@gmail.com>
2022-07-01 15:24:08 +0600

    add new bill modal done

--- src/components/Home/AddBill.js
+++ src/components/Home/AddBill.js
@@ -1,7 +1,53 @@
 import React from 'react';
 
-const AddBill = () => {
+const AddBill = ({ addBill, setAddBill }) => {
 
+    const handleAddBill = event => {
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const phone = event.target.phone.value;
+        const amount = event.target.amount.value;
+
+        // Module - 74
+        const bill = {
+            name,
+            email,
+            phone,
+            amount
+        }
+
+
+        fetch('http://localhost:5000/api/add-billing', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(bill)
+        })
+            .then(res => res.json())
+            .then(data => {
+
+                // booking success হলে
+                if (data.success) {
+                    // toast(`Appointment is set`)
+                }
+
+                // booking success না হলে
+                else {
+                    // toast.error(`Already have an appointment on`)
+                }
+
+                // refetch from react query
+                // refetch();
+
+
+                // to close the modal
+                setAddBill(false);
+            })
+
+    }
 
 
     return (
@@ -11,18 +57,18 @@ const AddBill = () => {
                 <div className="modal-box">
                     <label htmlFor="add-bill" className="btn btn-sm btn-circle absolute right-2 top-2 font-bold text-white">✕</label>
                     <h3 className="font-bold text-lg text-primary">Add a New Bill</h3>
-                    {/* <form onSubmit={handleAddBill} className='grid grid-cols-1 gap-4 justify-items-center mt-6'> */}
+                    <form onSubmit={handleAddBill} className='grid grid-cols-1 gap-4 justify-items-center mt-6'>
 
-                    <input type="text" name='name' placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='name' placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
 
-                    <input type="email" name='email' placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
+                        <input type="email" name='email' placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
 
-                    <input type="number" name='phone' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                        <input type="number" name='phone' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
 
-                    <input type="number" name='amount' placeholder="Paid Amount" className="input input-bordered w-full max-w-xs" />
+                        <input type="number" name='amount' placeholder="Paid Amount" className="input input-bordered w-full max-w-xs" />
 
-                    <input type="submit" value='Submit' className="btn btn-primary text-white w-full max-w-xs font-bold" />
-                    {/* </form> */}
+                        <input type="submit" value='Submit' className="btn btn-primary text-white w-full max-w-xs font-bold" />
+                    </form>
                 </div>
             </div>
         </div>
--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddBill from './AddBill';
 
 const HomeHeader = () => {
+    const [addBill, setAddBill] = useState(false);
+
+    const handleShow = () => setAddBill(true);
+
     return (
-        <div className='flex justify-between items-center p-2 w-11/12 mx-auto mt-12 mb-6 bg-secondary'>
-            <div className='flex items-center'>
-                <h3 className='px-8 text-xl text-white font-bold'>Billings</h3>
-                <div className="form-control">
-                    <div className="input-group h-4/5">
-                        <input type="text" placeholder="Search…" className="input input-bordered" />
-                        <button className="btn btn-small">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                        </button>
+        <div>
+            <div className='flex justify-between items-center p-2 w-11/12 mx-auto mt-12 mb-6 bg-secondary'>
+                <div className='flex items-center'>
+                    <h3 className='px-8 text-xl text-white font-bold'>Billings</h3>
+                    <div className="form-control">
+                        <div className="input-group h-4/5">
+                            <input type="text" placeholder="Search…" className="input input-bordered" />
+                            <button className="btn btn-small text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
+                <div>
+                    <label htmlFor="add-bill" className="btn btn-wide text-white font-bold" onClick={handleShow}>Add New Bill</label>
+                </div>
             </div>
-            <button className="btn btn-wide text-white">Add New Bill</button>
+            {
+                addBill && <AddBill
+                    addBill={addBill}
+                    setAddBill={setAddBill}
+                ></AddBill>
+            }
         </div>
     );
 };
